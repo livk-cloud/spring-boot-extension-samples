fdd7c15091aec1a524d9c48eb415c69b01af2381
livk <livk.cloud@gmail.com>
2026-04-03 11:34:45 +0800

    fix: 版本不兼容问题

--- spring-testcontainers-support/src/main/java/com/livk/testcontainers/DockerImageNames.java
+++ spring-testcontainers-support/src/main/java/com/livk/testcontainers/DockerImageNames.java
@@ -100,7 +100,7 @@ public class DockerImageNames {
 	}
 
 	public static DockerImageName pulsar() {
-		return pulsar(LATEST_TAG);
+		return pulsar("4.1.3");
 	}
 
 	public static DockerImageName pulsar(String tag) {
